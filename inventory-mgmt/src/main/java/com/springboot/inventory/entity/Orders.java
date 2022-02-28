36ff6301d46082a3c045c72c5a1e221cf4cb27ae
shivanip1512 <shivanipacharne10@gmail.com>
2022-02-28 15:14:43 +0530

    fixed date bug

--- inventory-mgmt/src/main/java/com/springboot/inventory/entity/Orders.java
+++ inventory-mgmt/src/main/java/com/springboot/inventory/entity/Orders.java
@@ -1,9 +1,14 @@
 package com.springboot.inventory.entity;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
 public class Orders {
 
@@ -12,7 +17,8 @@ public class Orders {
 	private Integer order_id;
 	private Integer item_id;
 	private Integer quantity;
-	private String Date;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date Date;
 
 	public Integer getOrder_id() {
 		return order_id;
@@ -38,30 +44,31 @@ public class Orders {
 		this.quantity = quantity;
 	}
 
-	public String getDate() {
-		return Date;
-	}
-
-	public void setDate(String date) {
-		Date = date;
-	}
-
-	@Override
-	public String toString() {
-		return "Order [order_id=" + order_id + ", item_id=" + item_id + ", quantity=" + quantity + ", Date=" + Date
-				+ "]";
-	}
-
 	public Orders() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Orders(Integer order_id, Integer item_id, Integer quantity, String date) {
+	public Date getDate() {
+		return Date;
+	}
+
+	public void setDate(Date date) {
+		Date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Orders [order_id=" + order_id + ", item_id=" + item_id + ", quantity=" + quantity + ", Date=" + Date
+				+ "]";
+	}
+
+	public Orders(Integer order_id, Integer item_id, Integer quantity, java.util.Date date) {
 		super();
 		this.order_id = order_id;
 		this.item_id = item_id;
 		this.quantity = quantity;
 		Date = date;
 	}
+
 }
